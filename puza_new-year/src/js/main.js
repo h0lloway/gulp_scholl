aa6a8173b6e233ed326b8428b94caff97fd81474
Dubrovin Maksim <gvozdb27@gmail.com>
2025-03-24 07:53:14 +0300

    work added

---
+++ puza_new-year/src/js/main.js
@@ -0,0 +1,100 @@
+// модальное окно
+
+const btns = document.querySelectorAll('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const modals = document.querySelectorAll('.modal')
+const closeBtns = document.querySelectorAll('.close-modal-btn')
+const body = document.body;
+const fixBlocks = document.querySelectorAll('.fix-block');
+
+
+let disableScroll = function () {
+  let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
+  let pagePosition = window.scrollY;
+  fixBlocks.forEach((el) => {
+    el.style.paddingRight = paddingOffset;
+  });
+  body.style.paddingRight = paddingOffset;
+  body.classList.add('disable-scroll');
+  body.dataset.position = pagePosition;
+  body.style.top = -pagePosition + 'px';
+}
+
+let enableScroll = function () {
+  let pagePosition = parseInt(document.body.dataset.position, 10);
+  body.style.top = 'auto';
+  body.classList.remove('disable-scroll');
+  fixBlocks.forEach((el) => {
+    el.style.paddingRight = '0px';
+  });
+  body.style.paddingRight = '0px';
+  window.scroll({ top: pagePosition, left: 0 });
+  body.removeAttribute('data-position');
+}
+
+closeBtns.forEach(closeBtn => {
+  closeBtn.addEventListener('click', function (event) {
+    modalOverlay.classList.remove('modal-overlay--visible');
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible');
+    });
+
+    enableScroll();
+
+  });
+});
+
+btns.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let path = e.currentTarget.getAttribute('data-path');
+
+
+    disableScroll();
+
+    modals.forEach((el) => {
+      el.classList.remove('modal-overlay--visible');
+    });
+
+    document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible')
+    modalOverlay.classList.add('modal-overlay--visible');
+  });
+});
+
+
+modalOverlay.addEventListener('click', (e) => {
+
+  if (e.target == modalOverlay) {
+
+    enableScroll();
+
+    modalOverlay.classList.remove('modal-overlay--visible');
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible');
+    });
+  }
+});
+
+
+function startTimer(duration, display) {
+	var timer = duration, minutes, seconds;
+	setInterval(function () {
+		hours = '00';
+		minutes = parseInt(timer / 60, 10);
+		seconds = parseInt(timer % 60, 10);
+
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
+
+		display.textContent = hours + ":" + minutes + ":" + seconds;
+
+		if (--timer < 0) {
+			timer = duration;
+		}
+	}, 1000);
+}
+
+window.onload = function () {
+	var fiveMinutes = 60 * 3,
+		display = document.querySelector('#countdown');
+	startTimer(fiveMinutes, display);
+};
